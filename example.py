834db3c6ef061adb4c6d1de26b08d84fba933a7f
Kyle Maxwell <krmaxwell@gmail.com>
2014-04-27 21:50:52 +0000

    Set up map correctly

--- example.py
+++ example.py
@@ -1,11 +1,12 @@
 import engine
 
-my_map = engine.Map([]) # no scenes at first, we will build these
+my_map = engine.Map(set())  # no scenes at first, we will build these
 
 cave_outside = engine.Scene('You see the entrance to a cave in the side of a hill.', set())
 my_map.add_scene(cave_outside)
 
-cave_entrance = engine.Scene('Your eyes adjust quickly to the darkness. A small chest sits off to the side.', set())
+cave_entrance = engine.Scene(
+    'Your eyes adjust quickly to the darkness. A small chest sits off to the side.', set())
 my_map.add_scene(cave_entrance)
 
 cave_outside.add_exit(cave_entrance)
