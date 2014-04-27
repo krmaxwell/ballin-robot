08d86b58a571b212b5b2a2854f753493f2692dca
Kyle Maxwell <krmaxwell@gmail.com>
2014-04-27 15:14:53 +0000

    Make it so.

---
+++ engine.py
@@ -0,0 +1,24 @@
+def Map(object):
+
+    def __init__(self):
+        pass
+
+
+def Scene(object):
+    
+    def __init__(self, description, props):
+        pass
+
+
+def Prop(object):
+
+    def __init__(self, description, location):
+        pass
+
+
+def Player(object):
+
+    def __init__(self, location):
+        pass
+
+    inventory = set()
