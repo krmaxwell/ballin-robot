b7af7ed4b6b44f8de21dac5f67e99a10cf69635e
Kyle Maxwell <krmaxwell@gmail.com>
2014-04-27 15:24:20 +0000

    Map and Scene objects

--- engine.py
+++ engine.py
@@ -1,13 +1,34 @@
 def Map(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, scenes):
+        assert isinstance(scenes, set)
+        self.scenes = scenes
+
+    def add_scene(scene):
+        assert isinstance(scene, Scene)
+        self.scenes.add(scene)
 
 
 def Scene(object):
     
     def __init__(self, description, props):
-        pass
+        assert isinstance(description, string)
+        assert isinstance(props, set)
+        self.description = description
+        self.props = props
+        self.exits = set()
+
+    def add_exit(self, next_scene):
+        assert isinstance(next_scene, Scene)
+        self.exits.add(next_scene)
+
+    def add_prop(self, prop):
+        assert isinstance(prop, Prop)
+        self.props.add(prop)
+
+    def remove_prop(self, prop):
+        assert prop in self.props
+        self.props.discard(prop)
 
 
 def Prop(object):
