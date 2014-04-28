882fe0208e43f469ed393cf688b15e429ea4be4c
Kyle Maxwell <kylem@xwell.org>
2014-04-27 20:00:02 -0500

    Load exits properly and list props in a scene

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ import ConfigParser
 
 DEBUG = True
 
+
 class Map(object):
 
     def __init__(self):
@@ -27,7 +28,9 @@ class Map(object):
         for each in map_db.items('Exits'):
             s_name, e_list = each
             for next_exit in e_list.split(','):
-                new_scene.add_exit(self.scenes[next_exit])
+                if DEBUG:
+                    print "Adding exit %r in %r" % (next_exit, s_name)
+                self.scenes[s_name].add_exit(self.scenes[next_exit])
         self.start_scene = self.scenes[map_db.get('Player', 'start_scene')]
 
 
@@ -54,7 +57,12 @@ class Scene(object):
 
     def enter(self):
         print self.description
-        print self.props
+        if len(self.props) > 0:
+            print "You see:"
+            for prop in self.props:
+                print "- %s" % prop
+        else:
+            print "Nothing notable here."
 
 
 class Prop(object):
@@ -81,4 +89,3 @@ class Player(object):
         assert new_location in self.location.exits
         self.location = new_location
         self.location.enter()
-
--- example.py
+++ example.py
@@ -6,3 +6,4 @@ my_map = engine.Map()
 my_map.load_scenes('example.map')
 
 me = engine.Player(my_map.start_scene)
+me.move(my_map.scenes['cave_entrance'])
