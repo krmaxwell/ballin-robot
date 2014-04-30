a3e51fd84f9a113e97dada2f75a672bf448f12db
Kyle Maxwell <krmaxwell@gmail.com>
2014-04-30 03:09:59 +0000

    Adds test for moving which does not pass right now

--- example.py
+++
@@ -1,8 +0,0 @@
-import engine
-
-
-
-my_map = engine.Map()
-my_map.load_scenes('example.map')
-
-me = engine.Player(my_map.start_scene)
--- tests/test.py
+++ tests/test.py
@@ -11,3 +11,10 @@ def test_start_player():
     my_map.load_scenes('example.map')
     me = engine.Player(my_map.start_scene)
     assert me.location == my_map.start_scene
+
+def test_move_player():
+    my_map = engine.Map()
+    my_map.load_scenes('example.map')
+    me = engine.Player(my_map.start_scene)
+    me.move(my_map.scenes['cave_entrance'])
+    assert me.location == my_map.scenes['cave_entrance']
